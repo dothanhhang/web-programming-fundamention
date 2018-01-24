25ca1eb8d017caa118793240a01f52b8a0ea1105
dothanhhang <phohanoi234@yahoo.com>
2018-01-24 18:09:42 +0700

    first express

---
+++ lesson-02-express-rounting/app.js
@@ -0,0 +1,17 @@
+'use strict'
+let express = require('express')
+
+let app = express()
+
+app.get('/', function () {
+
+})
+
+app.get('/about', function () {
+
+})
+
+app.listen(8000,function () {
+    console.log('app start at http://localhost:8000 ')
+})
+// Buoc 1 tao express
