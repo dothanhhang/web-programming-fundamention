10e1832cb9de22fa46c0850302f4c1049ef332c9
dothanhhang <phohanoi234@yahoo.com>
2018-01-26 15:25:47 +0700

    Buoc 2 get request for /

--- lesson-02-express-rounting/app.js
+++ lesson-02-express-rounting/app.js
@@ -2,8 +2,9 @@
 let express = require('express')
 let app = express()
 
-app.get('/', function () {
-
+app.get('/', function (req,res) {
+    console.log('get request for /')
+    res.status(302).send('hello')
 })
 
 app.get('/about', function () {
@@ -13,4 +14,5 @@ app.get('/about', function () {
 app.listen(8000,function () {
     console.log('app start at http://localhost:8000 ')
 })
-// Buoc 1 tao express
+// Buoc 2 tao them duong dan hien hello
+// Ham k ten
