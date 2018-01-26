75b5614a3048ac9f575d5a9bb791fcebf76e340e
dothanhhang <phohanoi234@yahoo.com>
2018-01-26 15:35:15 +0700

    Buoc 3 get request for /about'

--- lesson-02-express-rounting/app.js
+++ lesson-02-express-rounting/app.js
@@ -7,12 +7,13 @@ app.get('/', function (req,res) {
     res.status(302).send('hello')
 })
 
-app.get('/about', function () {
+app.get('/about', function (req,res) {
+    console.log('get request for /about')
+    res.status(200).send ('hello world!!! i am Sam')
 
 })
 
 app.listen(8000,function () {
     console.log('app start at http://localhost:8000 ')
 })
-// Buoc 2 tao them duong dan hien hello
-// Ham k ten
+// Buoc 3 get request for /about
